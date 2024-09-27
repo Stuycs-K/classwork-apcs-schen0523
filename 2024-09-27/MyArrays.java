d8dd93a6dd82dfbb9d0575dd622b5b952bf11b58
schen0523 <schen65@stuy.edu>
2024-09-27 10:42:16 -0500

    arraytostring

---
+++ 2024-09-27/MyArrays.java
@@ -0,0 +1,23 @@
+public class MyArrays {
+  public static void main (String[] args) {
+    //test case for arrayToString
+    int [] testArray1 = new int[]{1, 2, 3};
+    System.out.println(arrayToString(testArray1));
+
+
+  }
+  public static String arrayToString (int[] nums) {
+    String result = "[";
+    for (int i = 0; i < nums.length; i++) {
+      result += nums[i];
+      if (i < nums.length - 1) {
+        result += ", ";
+      }
+    }
+    return result + "]";
+  }
+
+
+
+
+}
