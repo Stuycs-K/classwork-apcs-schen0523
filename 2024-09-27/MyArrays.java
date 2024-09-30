c21c35bac93c80cb0ccee7c77271d75ab907e539
Serena Chen <schen65@stuy.edu>
2024-09-30 00:35:59 -0400

    returnCopy test cases

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,10 +1,32 @@
 public class MyArrays {
   public static void main (String[] args) {
     //test case for arrayToString
-    int [] testArray1 = new int[]{1, 2, 3};
+    int [] testArray1 = new int[]{1, 2, 3, 4};
+    System.out.println(arrayToString(testArray1));
+    testArray1 = new int[0];
     System.out.println(arrayToString(testArray1));
 
     //test cases for returnCopy
+    int [] original = new int[]{0, 1, 2, 3, 4, 5};
+    System.out.println("original array: " + arrayToString(original));
+    int [] copy = returnCopy(original);
+    System.out.println("copy array: " + arrayToString(copy));
+    //checking to see if the values in the arrays match
+    boolean result = true;
+    for(int i = 0; i < original.length; i++) {
+      if (original[i] != copy[i]) {
+        result = false;
+        i = original.length;
+        System.out.println("are the values the same? no");
+      }
+    }
+    if (result) {
+      System.out.println("are the values the same? yes");
+    }
+    //modify one array to see if the other one changes as well
+    original[0] = 1; 
+    System.out.println("are the arrays the same? " + (original[0] == copy[0]));
+
 
     //test cases for concatArray
 
@@ -19,6 +41,9 @@ public class MyArrays {
       }
     }
     return result + "]";
+  }
+  public static int[] returnCopy(int[] ary) {
+    return ary;
   }
 
 
