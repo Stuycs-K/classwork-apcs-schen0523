875b11c5b41e87ba71ecc4b2d7f62b30dbe7c7dd
Serena Chen <schen65@stuy.edu>
2024-10-01 03:55:10 -0400

    null fix

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -2,67 +2,92 @@
 //Serena Chen; schen65@stuy.edu
 //Raymond Chen; rchen60@stuy.edu
 
+public class ArrayMethods {
 //2. Copy your arrToString method from before.
 /**Return a String that represets the array in the format:
 * "[2, 3, 4, 9]"
 * Note the comma+space between values, and between values
 */
-public static String arrToString(int[]ary){
-  String result = "[";
-  for (int i = 0; i < ary.length; i++) {
-    result += ary[i];
-    if (i < ary.length - 1) {
-      result += ", ";
+  public static String arrToString(int[]ary){
+    String result = "[";
+    for (int i = 0; i < ary.length; i++) {
+      result += ary[i];
+      if (i < ary.length - 1) {
+        result += ", ";
+      }
     }
+    return result + "]";
   }
-  return result + "]";
-}
 
-//3. Write arrToString, with a 2D array parameter.
-//Note: Different parameters are allowed with the same function name.
-/**Return a String that represets the 2D array in the format:
-  * "[[2, 3, 4], [5, 6, 7], [2, 4, 9]]"
-  * Note the comma+space between values, and between arrays
-  * You are encouraged to notice that you may want to re-use
-  * previous code, but you should NOT duplicate that code. (Don't copy/paste or retype it)
+  //3. Write arrToString, with a 2D array parameter.
+  //Note: Different parameters are allowed with the same function name.
+  /**Return a String that represets the 2D array in the format:
+    * "[[2, 3, 4], [5, 6, 7], [2, 4, 9]]"
+    * Note the comma+space between values, and between arrays
+    * You are encouraged to notice that you may want to re-use
+    * previous code, but you should NOT duplicate that code. (Don't copy/paste or retype it)
   */
-public static String arrToString2(int[][]ary){
-  String result = "[";
-  for (int i = 0; i < ary.length; i++) {
-    result += arrToString(ary[i]);
-    if (i < ary.length - 1) {
-      result += ", ";
+  public static String arrToString(int[][]ary){
+    //this should use arrToString(int[])
+    String result = "[";
+    for (int i = 0; i < ary.length; i++) {
+      if (ary[i] == null) {
+        result += "null";
+      }
+      else {
+      result += arrToString(ary[i]);
+      }
+      if (i < ary.length - 1) {
+        result += ", ";
+      }
     }
+    return result + "]";
   }
-  //this should use arrToString(int[])
-  return result + "]";
-}
 
-/*Return the sum of all of the values in the 2D array */
-public static int arr2DSum(int[][]nums){
-  //use a nested loop to solve this
-  return null;//place holder return value so it compiles.
-}
+  /*Return the sum of all of the values in the 2D array */
+  public static int arr2DSum(int[][]nums){
+    //use a nested loop to solve this
+    int sum = 0;
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = 0; j < nums[i].length; j++) {
+        sum += nums[i][j];
+      }
+    }
+    return sum;//place holder return value so it compiles.
+  }
 
-/**Rotate an array by returning a new array with the rows and columns swapped.
-  * You may assume the array is rectangular and neither rows nor cols is 0.
-  * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
+  /**Rotate an array by returning a new array with the rows and columns swapped.
+    * You may assume the array is rectangular and neither rows nor cols is 0.
+    * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
   */
-public static int[][] swapRC(int[][]nums){
-  return new int[1][1];
-}
+  public static int[][] swapRC(int[][]nums){
+    return new int[1][1];
+  }
 
-public static void main(String[] args) {
-  // arrToString for 2D array test cases
-  int[][] testArray = new int[][];
-  System.out.println("Expected [[]], result: " + arrToString2(testArray));
+  public static void main(String[] args) {
+    //arrToString for 2D arrays test cases
+    int[][] testArray = new int[2][4];
+    System.out.println("expected: [[0, 0], [0, 0, 0, 0]]");
+    System.out.println("result: " + arrToString(testArray));
 
-  testArray = int[2][];
-  System.out.println("Expected [[], []], result: " + arrToString2(testArray));
+    testArray = new int[2][];
+    System.out.println("expected: [null, null]");
+    System.out.println("result: " + arrToString(testArray));
 
-  testArray = int {{1, 2, 3, 4}, {5, 6, 7, 8}};
-  System.out.println("Expected [[1, 2, 3, 4], [5, 6, 7, 8]], result: " + arrToString2(testArray));
+    testArray = new int[][] {{1, 2, 3, 4}, {5, 6, 7, 8}};
+    System.out.println("expected: [[1, 2, 3, 4], [5, 6, 7, 8]]");
+    System.out.println("result: " + arrToString(testArray));
 
-  testArray = int {{1, 2, 3, 4}, {5, 6}, {7, 8, 9}};
-  System.out.println("Expected [[1, 2, 3, 4], [5, 6], [7, 8, 9]], result: " + arrToString2(testArray));
+    testArray = new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    System.out.println("expected: [[1, 2, 3], [4, 5, 6], [7, 8, 9]]");
+    System.out.println("result: " + arrToString(testArray));
+
+    testArray = new int[][] {{1, 2, 3, 4}, {5, 6}, {7, 8, 9}};
+    System.out.println("expected: [[1, 2, 3, 4], [5, 6], [7, 8, 9]]");
+    System.out.println("result: " + arrToString(testArray));
+
+    //arr2DSum test cases
+    //testArray = ;
+  }
+
 }
