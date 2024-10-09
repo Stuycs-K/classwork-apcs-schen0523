7735d29a10e26e5e643165cf1dd3a3111b1b5545
Serena Chen <schen65@stuy.edu>
2024-10-09 01:00:04 -0400

    replaceNegative

--- 2024-10-07-2DArrayMethods/ArrayDemo.java
+++ 2024-10-07-2DArrayMethods/ArrayDemo.java
@@ -161,7 +161,18 @@ public class ArrayDemo{
   //that negative with the value 1
   //-All other negatives replace with 0
   public static void replaceNegative(int[][] vals){
-
+    for(int i = 0; i < vals.length; i++) {
+      if (vals[i].length != 0) {
+        for (int j = 0; j < vals[i].length; j ++) {
+          if (vals[i][j] < 0 && i == j) {
+            vals[i][j] = 1;
+          }
+          else if (vals[i][j] < 0) {
+            vals[i][j] = 0;
+          }
+        }
+      }
+    }
   }
 
   //4. Make a copy of the given 2d array.
