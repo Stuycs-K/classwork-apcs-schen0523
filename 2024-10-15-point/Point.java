defaba98b79888b87c13a80f5dcd550a046b8f58
schen0523 <schen65@stuy.edu>
2024-10-15 11:31:15 -0400

    second point constructor

--- 2024-10-15-point/Point.java
+++ 2024-10-15-point/Point.java
@@ -8,6 +8,8 @@ public class Point{
   //Initialize this Point to have the same values as the other Point.
   public Point(Point other){
     //COMPLETE THIS SECOND
+    x = other.getX();
+    y = other.getY();
     //Then write test cases.
   }
 
