e9f0c1313a5e34fc242ab07046d3782603d5e88a
schen0523 <schen65@stuy.edu>
2024-11-04 11:45:04 -0500

    triangletester method

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,1 +1,36 @@
-public static void
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TriangleTester {
+  public static void main (String[] args) {
+    //System.out.println(isTri(5, 5, 2));
+    System.out.println(countTrianglesA("inputTri.txt"));
+  }
+
+  public static int countTrianglesA(String filename) {
+    int counter = 0;
+    int x, y, z = 0;
+    File file = new File(filename);//1
+    Scanner input = new Scanner(filename);
+    while(input.hasNextLine()) {
+      x = input.nextInt();
+      y = input.nextInt();
+      z = input.nextInt();
+      if (isTri(x, y, z)) {
+          counter++;
+      }
+    }
+
+
+
+
+    input.close();
+    return counter;
+  }
+
+  public static boolean isTri(int a, int b, int c) {
+    return (a + b > c && a + c > b && b + c > a);
+  }
+
+}
