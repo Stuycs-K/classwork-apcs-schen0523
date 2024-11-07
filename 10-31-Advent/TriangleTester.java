304794eb254ed7ca0888efa52012a866798f8995
Serena Chen <schen65@stuy.edu>
2024-11-07 01:15:28 -0500

    debug

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -5,17 +5,19 @@ import java.util.Scanner;
 public class TriangleTester {
   public static void main (String[] args) {
     //System.out.println(isTri(5, 5, 2));
+    //System.out.println(countTrianglesA("inputTri.txt"));
     System.out.println(countTrianglesA("inputTri.txt"));
   }
 
   public static int countTrianglesA(String filename) {
     int counter = 0;
     int x, y, z = 0;
+
     try {
       File file = new File(filename);//1
       Scanner input = new Scanner(file);
 
-      while(input.hasNextLine()) {
+      while(input.hasNextInt()) {
         x = input.nextInt();
         y = input.nextInt();
         z = input.nextInt();
@@ -23,8 +25,10 @@ public class TriangleTester {
           counter++;
         }
       }
+
       input.close();
       return counter;
+    
     } catch (FileNotFoundException ex) {
       System.out.println("File not found");
       return 0; 
@@ -35,6 +39,38 @@ public class TriangleTester {
   public static boolean isTri(int a, int b, int c) {
     return (a + b > c && a + c > b && b + c > a);
   }
+
+  public static int countTrianglesB(String filename) {
+    int row, column = 0;
+    int counter = 0;
+    int x1, x2, x3, y1, y2, y3, z1, z2, z3 = 0;
+
+    try {
+      File file = new File(filename);//1
+      Scanner input = new Scanner(file);
+
+      while(input.hasNextInt()) {
+        x1 = input.nextInt();
+        x2 = input.nextInt();
+        x3 = input.nextInt();
+        y1 = input.nextInt();
+        y2 = input.nextInt();
+        y3 = input.nextInt();
+        z1 = input.nextInt();
+        z2 = input.nextInt();
+        z3 = input.nextInt();
+        if (isTri(x1, y1, z1) || isTri(x2, y2, z2) || isTri(x3, y3, z3)) {
+          counter++;
+        }
+      }
+
+      input.close();
+      return counter;
     
+    } catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+      return 0; 
+    }
+  }
 
 }
