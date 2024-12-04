e005a24840e35921333c8da101ed78f68181ecd6
schen0523 <schen65@stuy.edu>
2024-12-04 11:21:10 -0500

    made new classwork Color Screen

---
+++ 2024-12-04-ColorScreen/Driver.java
@@ -0,0 +1,18 @@
+public class Driver {
+
+  public static void main (String[] args) {
+
+    for(int i = 0; i < 80; i++) {
+      if (i == 0 || i == 79) {
+        //print whole row
+      }
+      else {
+        //print only beginning and end
+      }
+    }
+  }
+
+  public static int[] makeRandomArray(len) {
+    int[] result = new
+  }
+}
